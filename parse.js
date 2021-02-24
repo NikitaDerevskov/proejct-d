e6fce1c3dce5c1829c7a080a44d9c90ba0bfd6be
Nikita Derevskov <nikitaderevskov@gmail.com>
2021-02-24 23:40:58 +0300

    parse.js: start work on tutu and arch

--- parse.js
+++ parse.js
@@ -1,15 +1,41 @@
 const puppeteer = require('puppeteer');
 
-const getHtml = async url => {
+const tutuGetHtml = async url => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     const html = await page.goto(url).then(() => page.content());
-    return html;
+    return {html, browser};
+}
+
+const tutuGetData = async url => {
+    // TODO Launch browser and send it to getfirstPage.
+    // 26.02.2021 - this can help you to better parse
+    let tutuData = await tutuGetHtml(url)
+    return tutuData.html;
+}
+
+const parse = async ({url, parseFn}) => {
+    return await parseFn(url);
 }
 
 const main = async () => {
-    const url = 'https://www.reddit.com';
-    console.log('html', await getHtml(url).catch(x => console.log('error', x)));
+    const resourses = {
+        tutu: {url: "https://www.tutu.ru/", parseFn: tutuGetData},
+        ali: {}, // TODO
+        ps4Store: {} // TODO
+    }
+
+    /*
+    *  We have some resources (tutu, ali, ps4Strore and etc).
+    *  All things in project - just modules.
+    *
+    *
+    * */
+
+    const tutuData = await parse(resourses.tutu)
+
+    console.log(tutuData)
+    console.log('hey boy')
 }
 
 main();
